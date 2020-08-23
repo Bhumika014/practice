f49aa40acde3fe74cc0b13386d0e7d61baec862e
Bhumika014 <bhumikapatel014@gmail.com>
2020-08-23 14:36:40 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace username
+{
+    class Program
+    {
+        private static string userName;
+
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 9;
+
+            int total = x + y;
+            int total1 = x - y;
+            int total2 = x * y;
+
+            Console.WriteLine(total);
+            Console.WriteLine(total1);
+            Console.WriteLine(total2);
+
+
+            Console.WriteLine("Enter the username: ");
+
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Username is: " + userName );
+
+            comparision.comparisionprogram();
+            
+        }
+    }
+}
---
+++ comparision.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace username
+{
+    class comparision
+    {
+        
+
+        public static void comparisionprogram()
+        {
+
+            Console.WriteLine("Enter number a:");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter number b:");
+
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine("a is greater than b");
+            }
+            else if (a < b)
+            {
+                Console.WriteLine("a is  less than b");
+            }
+            else
+                Console.WriteLine("a is equal to b");
+
+
+            return;
+            
+        }
+    }
+}
